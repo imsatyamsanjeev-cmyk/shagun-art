c884986a5b8d0a7f3efacc275eac7be89194800d
imsatyamsanjeev-cmyk <im.satyamsanjeev@gmail.com>
2026-07-04 21:30:47 +0530

    Reordered page sections to render Gallery immediately below the Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -51,9 +51,9 @@ function App() {
       <Navbar />
       <main>
         <Hero />
+        <Gallery />
         <About />
         <Services />
-        <Gallery />
         <WhyChoose />
         <Booking />
         <Testimonials />
